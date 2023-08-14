3ff96cd3742dba4277074fb5962597735048330c
Justin Carroll <50720444+YauxtnKrl@users.noreply.github.com>
2023-08-14 14:59:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function GetDate() {
     let epochDate = Date.now();
     let element = document.getElementById("BodyParagraph")
 
-    element.textContent = element.innerHTML + epochDate;
+    element.textContent = epochDate;
 }
 
 GetDate();
